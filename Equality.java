0457aa9221eaf3eb97ce46f32feea90444293a04
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-20 15:56:40 +0530

    Add files via upload

---
+++ Equality.java
@@ -0,0 +1,20 @@
+
+public class Equality {
+    public static void main(String[] args){
+        printEqual(1,1,1);
+        printEqual(1,2,1);
+        printEqual(-1,-1,-1);
+    }
+
+    public static void printEqual(int a,int b,int c){
+        if(a<0 || b<0 || c<0){
+            System.out.println("Invalid value");
+        } else if (a == b ==c){
+            System.out.println("all numbers are equal");
+        } else if (a!=b!=c){
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
